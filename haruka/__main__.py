c64f261ef9f63d634b910cbd3d90757ed3e92b8d
Kei <73867910+KeinShin@users.noreply.github.com>
2021-01-16 10:53:57 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,11 +29,9 @@ I'm group manager bot and Anti-spam for Lightning
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Krish](t.me/krish1303y)
-
 Click /help or Help button below to find out more about how to use me to my full potential.
 
- Add me to a group by clicking [here](http://t.me/kenshingrouphelpbot?startgroup=true).
+ Add me to a group by clicking [Here](http://t.me/kenshingrouphelpbot?startgroup=true).
 """
 
 
